e5473ede02dc227ff32ee4aabf0ebe38c7a7a7d5
Anıl Göktaş <goktas.anil@gmail.com>
2016-06-19 19:30:49 +0300

    Header changes

--- ReachabilityStatus/ReachabilityStatus.h
+++ ReachabilityStatus/ReachabilityStatus.h
@@ -16,4 +16,4 @@ FOUNDATION_EXPORT const unsigned char ReachabilityStatusVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <ReachabilityStatus/PublicHeader.h>
 
-
+@import ReachabilitySwift;
